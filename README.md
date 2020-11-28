57cefe7dddd075e64ac1a5018111e5afc6087c83
yumitomo <yaonuma@umich.edu>
2020-11-28 15:23:40 -0500

    First commit. Tool to iteratively download thumbnails, with random VPN connections

---
+++ README.md
@@ -0,0 +1,8 @@
+# Fashion_Runway_Show_Archive
+A webscraped archive of catwalk images using Beautiful Soup and Selenium.
+
+
+Abstract:
+
+There is no data in the repository. You must configure your environment, then run main.py. It will generate a "data" folder containing M thumbnail images (106x160 pixels, jpeg format) from thousands of designers (not commercially oriented companies for mass consumers), for women and men, from 1995 to 2020. This dataset is intended to be used in conjunction with a pretrained/modified convolutional neural network that can take a new catwalk image, perform segmentation to separate the clothing from the model, then run it against this database, which is also to be segmented in the same way, and return an "originality index", which is yet to be mathematically defined, and a list of the top correlating images (designs) from the archives. Since fashion trends are rarely totally new, but often cyclical over decades, there should, in principle, exist past images/designs which a human would agree are highly correlated. The end product would ideally be an AI manifestation of Diet Prada (https://www.dietprada.com/). Not as powerful as the two humans that run it, but still useful.
+
--- main.py
+++ main.py
@@ -2,12 +2,96 @@ import datetime
 import os
 import time
 from urllib.parse import urlparse
+from urllib3.exceptions import MaxRetryError
 
 import requests
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from tqdm import tqdm
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+from expressvpn.wrapper import random_connect
+from expressvpn import wrapper
+import logging
+logging.basicConfig(filename='log.dat')
+
+TERMCOLOR = True
+if TERMCOLOR:
+    from termcolor import colored
+
+
+def printm(text, color='white', switch=False):
+    # wrapper for standard print function
+    if switch:
+        print(colored(text, color))
+    else:
+        print(text)
+    return
+
+
+def change_ip():
+    max_attempts = 10
+    attempts = 0
+    while True:
+        attempts += 1
+        try:
+            logging.info('GETTING NEW IP')
+            wrapper.random_connect()
+            logging.info('SUCCESS')
+            return
+        except Exception as e:
+            if attempts > max_attempts:
+                logging.error('Max attempts reached for VPN. Check its configuration.')
+                logging.error('Browse https://github.com/philipperemy/expressvpn-python.')
+                logging.error('Program will exit.')
+                exit(1)
+            logging.error(e)
+            logging.error('Skipping exception.')
+
+
+def rotate_proxy():
+
+    options = webdriver.ChromeOptions()
+    # options.add_argument("start-maximized")
+    options.add_argument("--headless")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get("https://sslproxies.org/")
+
+    driver.execute_script("return arguments[0].scrollIntoView(true);", WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.XPATH,
+                                          "//table[@class='table table-striped table-bordered dataTable']//th[contains(., 'IP Address')]"))))
+    ips = [my_elem.get_attribute("innerHTML") for my_elem in WebDriverWait(driver, 5).until(
+        EC.visibility_of_all_elements_located((By.XPATH,
+                                               "//table[@class='table table-striped table-bordered dataTable']//tbody//tr[@role='row']/td[position() = 1]")))]
+    ports = [my_elem.get_attribute("innerHTML") for my_elem in WebDriverWait(driver, 5).until(
+        EC.visibility_of_all_elements_located((By.XPATH,
+                                               "//table[@class='table table-striped table-bordered dataTable']//tbody//tr[@role='row']/td[position() = 2]")))]
+    driver.quit()
+    proxies = []
+    for i in range(0, len(ips)):
+        proxies.append(ips[i] + ':' + ports[i])
+    print(proxies)
+    for i in range(0, len(proxies)):
+        try:
+            print("Proxy selected: {}".format(proxies[i]))
+            options = webdriver.ChromeOptions()
+            options.add_argument('--proxy-server={}'.format(proxies[i]))
+            driver = webdriver.Chrome(options=options, executable_path=r'C:\WebDrivers\chromedriver.exe')
+            driver.get("https://www.whatismyip.com/proxy-check/?iref=home")
+            if "Proxy Type" in WebDriverWait(driver, 5).until(
+                    EC.visibility_of_element_located((By.CSS_SELECTOR, "p.card-text"))):
+                break
+        except Exception:
+            driver.quit()
+    print("Proxy Invoked")
 
 
 def is_valid(url):
@@ -24,11 +108,12 @@ def is_valid(url):
     return is_valid
 
 
-def get_links(url, show, number):
+def get_links(url, show, number, root_data):
     """
     A function to get links to all the thumbnails in a url-specified runway show
 
     :param url: <str> url the page that has all of the thumbnails
+
     :return imgs: <list<str>> list of full url paths to the thumbails. also outputs a csv file containing the links
     :return show_name: <str> name of the show corresponding to the images
     """
@@ -91,7 +176,6 @@ def get_links(url, show, number):
             continue
 
     # create the root data directory
-    root_data = 'data/'
     if not os.path.exists(root_data):
         try:
             os.mkdir(root_data)
@@ -101,7 +185,7 @@ def get_links(url, show, number):
             print("\t[INFO] Successfully created the directory {} ".format(os.path.abspath(root_data)))
 
     # Create the directory after checking if it already exists or not
-    dir_name = root_data + show.replace('/', '+') + '_' + str(number)
+    dir_name = root_data + show.replace('/', '+')
     if not os.path.exists(dir_name):
         try:
             os.mkdir(dir_name)
@@ -111,13 +195,13 @@ def get_links(url, show, number):
             print("\t[INFO] Successfully created the directory {} ".format(os.path.abspath(dir_name)))
 
     # put images from who in its own directory
-    for img in imgs:
+    for img in tqdm(imgs):
         download(img, dir_name)
 
     # Write the links to the image pages to a file
     f = open("{}/{}.csv".format(dir_name, show_name + '_' + str(number + 1)), 'w')
     f.write(",\n".join(imgs))
-    print("\t[INFO] Successfully created the file {}.csv with {} links".format(show_name, len(imgs)))
+    printm("\t[INFO] Successfully created the file {}.csv with {} links".format(show_name, len(imgs)),color='blue',switch=True)
 
     return imgs
 
@@ -134,6 +218,7 @@ def download(url, pathname):
     # if path doesn't exist, make that path dir
     if not os.path.isdir(pathname):
         os.makedirs(pathname)
+
     # download the body of response by chunk, not immediately
     try:
         response = requests.get(url, stream=True)
@@ -145,17 +230,33 @@ def download(url, pathname):
         filename = os.path.join(pathname, url.split("/")[-1])
 
         # progress bar, changing the unit to bytes instead of iteration (default by tqdm)
-        progress = tqdm(response.iter_content(1024), f"\tDownloading {filename}", total=file_size, unit="B",
-                        unit_scale=True, unit_divisor=1024)
+        # progress = tqdm(response.iter_content(64), f"\tDownloading {filename}", total=file_size, unit="B",
+        #                 unit_scale=True, unit_divisor=1024)
         with open(filename, "wb") as f:
 
-            for data in progress:
+            for data in response.iter_content(64):
                 # write data read to the file
                 f.write(data)
                 # update the progress bar manually
-                progress.update(len(data))
-    except requests.exceptions.InvalidSchema:
+                # progress.update(len(data))
+
+    except requests.exceptions.InvalidSchema as ae:
+        # logging.info('No connection adapters were found.')
+        # logging.error(ae)
         pass
+
+    except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError,
+            requests.exceptions.ConnectionError, MaxRetryError) as be:
+        logging.info('BANNED EXCEPTION in __MAIN__')
+        logging.info(be)
+        logging.info('Lets change our PUBLIC IP GUYS!')
+        change_ip()
+        print('\t\tSleeping after ip change for 1 hour.')
+        time.sleep(3600)
+
+    except Exception as ce:
+        logging.error('Exception raised.')
+        logging.error(ce)
 
 
 def get_all_show_images(url, gender):
@@ -170,7 +271,8 @@ def get_all_show_images(url, gender):
     """
 
     # print validity of url provided
-    print('URL validity [bool] : ', is_valid(url), '\n')
+    out = 'URL validity [bool] : '+str(is_valid(url))+str('\n')
+    printm(out, color='green', switch=False)
 
     # Define Chrome options to open the window in maximized mode
     options = webdriver.ChromeOptions()
@@ -190,7 +292,15 @@ def get_all_show_images(url, gender):
 
     N_mens_total = 0
     for page in range(pages + 1):
-        print('Getting runway shows from Page ', page + 1)
+
+        #maybe avoid max HTTP requests
+        # print(page%20)
+        # if page > 10 and page % 40 == 0:
+        #     print('\t\tSleeping after a page multiple of 40 for an hour.')
+        #     time.sleep(3600)
+
+        out = '\nGetting runway shows from Page '+str(page + 1)
+        printm(out, color='green', switch=True)
         if page > 0:
             try:
                 link = driver.find_element_by_link_text(str(page + 1))
@@ -204,14 +314,23 @@ def get_all_show_images(url, gender):
         shows = soup_show.find_all('h3')
         shows = [item.getText() for item in shows if ' - ' in item.getText()]
 
+        root_data = 'data/'
         for i, show in enumerate(shows):
-            print('\tGetting links for ', show)
-            link = driver.find_element_by_link_text(show)
-            link.click()
-            imgs = get_links(driver.current_url, show, i)
-            n_imgs = len(imgs)
-            N_mens_total += n_imgs
-            driver.back()
+            if os.path.isdir(root_data+show.replace('/','+')):
+                # this block is here because, unfortunately, runway shows that contain a lot of images
+                # are carried onto new pages under the same name. logic could be added to take this into
+                # account, but there are already a sufficient number of images to start
+                continue
+            try:
+                print('\n\tGetting links for ', show)
+                link = driver.find_element_by_link_text(show)
+                link.click()
+                imgs = get_links(driver.current_url, show, i, root_data)
+                n_imgs = len(imgs)
+                N_mens_total += n_imgs
+                driver.back()
+            except NoSuchElementException:
+                pass
 
     print('Downloaded ', N_mens_total, """ images from men's runway shows.""")
 
@@ -219,6 +338,8 @@ def get_all_show_images(url, gender):
 
 
 if __name__ == '__main__':
-    home_page = 'http://www.firstview.com/collection.php'
+
     get_all_show_images(home_page, 'Mens')
-    get_all_show_images(home_page, 'Womens')
+    # get_all_show_images(home_page, 'Womens')
+
+    # rotate_proxy()
